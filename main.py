de7f43d7d0c7f9ef0f4c0de39b3e5683bbb8c569
Cognithos <165255787+aiman-misgar@users.noreply.github.com>
2025-05-05 11:06:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,21 +15,3 @@ app.include_router(router)
 def read_root():
     return {"message": "Welcome to the Ancient DNA API!"}  
 
-=======
-from fastapi import FastAPI
-from app.routes import router  # import your router
-
-app = FastAPI(
-    title="Ancient DNA Sequence Analysis API",
-    description="API to analyze and compare DNA sequences from ancient remains.",
-   
-)
-
-app.include_router(router)
-
-# Optional root
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to the Ancient DNA API!"}  
-
-       
